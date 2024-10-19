72102cd3994034884b5f4cdd4b10d76fd4064192
Lee Guiberson <lee.guiberson@gmail.com>
2024-10-19 15:49:09 -0500

    Added map
    Co-authored-by: OrigamiPaper225 <OrigamiPaper225@users.noreply.github.com>
    Co-authored-by: Aidan <AidanG1@users.noreply.github.com>
    Co-authored-by: Sanya Garg <sanyagarg22@users.noreply.github.com>

---
+++ frontend-thea-aa/src/lib/maps/Control.svelte
@@ -0,0 +1,45 @@
+<!-- <script lang="ts">
+	import L from 'leaflet'
+	import { getContext, onDestroy } from 'svelte'
+	let classNames = undefined
+	export { classNames as class }
+	class Control extends L.Control {
+		constructor(el, position) {
+			super({ position })
+			this.el = el
+		}
+		onAdd() {
+			return this.el
+		}
+		onRemove() {}
+	}
+
+    interface ControlProps {
+        position: 'topleft' | 'topright' | 'bottomleft' | 'bottomright'
+    }
+
+	/** position: 'topleft' | 'topright' | 'bottomleft' | 'bottomright' */
+	export let position
+
+	/** The control instance created by this component */
+	export let control = undefined
+	const map = getContext('map')()
+
+	function createControl(container) {
+		control = new Control(container, position).addTo(map)
+		return {
+			destroy() {
+				control.remove()
+				control = undefined
+			}
+		}
+	}
+</script>
+
+<div style="display:hidden">
+	<div use:createControl class={classNames}>
+		{#if control}
+			<slot {control} />
+		{/if}
+	</div>
+</div> -->
