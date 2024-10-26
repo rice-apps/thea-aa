60e955f71aba9d008bd9d30e1bffbfa7844e45ea
vizdiz <vismayravikumar@gmail.com>
2024-10-26 15:35:48 -0500

    Created the Django model for Emission Events and initialized associated variables

--- backend-thea-aa/main/models.py
+++ backend-thea-aa/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class EmissionEvents(models.Model):
+
+    # Initialize fields for the Django model storing emission events
+    re_name = models.TextField()
+    physical_location = models.TextField()
+    start_date_time = models.DateTimeField()
+    end_date_time = models.DateTimeField()
+    contaminant = models.TextField()
+    estimated_quantity = models.DecimalField()
+    emissions_limit = models.DecimalField
+    limit_units = models.TextField()
+    hours_elapsed = models.DecimalField()
+    emissions_rate = models.DecimalField()
+    flag = models.BinaryField()
