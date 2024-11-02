49c4e99ba19d97b4f488bf14a44bf67027ae0635
Sanya Garg <sanya.garg@gmail.com>
2024-11-02 15:49:39 -0500

    Continued styling & adding to home page

--- frontend-thea-aa/src/lib/components/ContaminatedSites.svelte
+++ frontend-thea-aa/src/lib/components/ContaminatedSites.svelte
@@ -7,26 +7,27 @@
     let amountToShow = $state(3)
 </script>
 
-<h3>Contaminated Sites</h3>
+<div>
+    <h3>Contaminated Sites</h3>
 
-<table>
-    <thead>
-        <tr>
-        <th>Status</th>
-        <th>Site</th>
-        <th>Hazard Score</th>
-        </tr>
-    </thead>
-    <tbody>
-        {#each props.items.slice(0, amountToShow) as item}
-        <tr>
-            <td>{item.status}</td>
-            <td>{item.name}</td>
-            <td>{item.hazardScore}</td>
-        </tr>
-        {/each}
-    </tbody>
-</table>
+    <table>
+        <thead>
+            <tr>
+            <th>Status</th>
+            <th>Site</th>
+            <th>Hazard Score</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each props.items.slice(0, amountToShow) as item}
+            <tr>
+                <td>{item.status}</td>
+                <td>{item.name}</td>
+                <td>{item.hazardScore}</td>
+            </tr>
+            {/each}
+        </tbody>
+    </table>
 
-<LoadMore onclick={() => amountToShow += 3} />
-
+    <LoadMore onclick={() => amountToShow += 3} />
+</div>
--- frontend-thea-aa/src/routes/+page.svelte
+++ frontend-thea-aa/src/routes/+page.svelte
@@ -4,31 +4,48 @@
 	import ContaminatedSites from '$lib/components/ContaminatedSites.svelte';
 	import Map from '$lib/components/Map.svelte'
 	import { Button } from '$lib/components/ui/button'
+	import { Building2 } from 'lucide-svelte';
+	import { Cloudy } from 'lucide-svelte';
+	import { Wind } from 'lucide-svelte';
+	import { ListFilter } from 'lucide-svelte';
+
 
   let { data } = $props()
 
-	let showContaminated= $state(true)
-	let showEmissionEvents= $state(true)
+	let currentView = $state('');
+
+	let height = $state(0);
+	
 </script>
 
-<main class="flex">
-	<aside>
-		<input placeholder="search" />
-		<div>
-			<Button on:click={()=>{showContaminated=!showContaminated}}>Contaminated Sites</Button>
-			<Button on:click={()=>{showEmissionEvents=!showEmissionEvents}}>Emission Events</Button>
+<svelte:window bind:innerHeight={height} />
+<main class="flex h-full p-10 space-x-9">
+	<aside class="w-1/3 bg-white rounded-lg shadow-lg p-8">
+		<input placeholder="Search Address or Site" class="mb-2 p-2 border rounded" />
+		<Button>Filter<ListFilter></ListFilter></Button>
+		<div class = "overflow-x-auto whitespace-nowrap space-x-2 flex py-2">
+			<Button on:click={() => currentView = 'superfund'} class=""><Building2></Building2>Superfund</Button>
+			<Button on:click={() => currentView = 'emission'}><Cloudy></Cloudy>Emission Events</Button>
+			<Button on:click={() => currentView = 'air quality'}><Wind></Wind>Air Quality</Button>
+			<Button on:click={() => currentView = 'air quality'}><Wind></Wind>Air Quality</Button>
+			<Button on:click={() => currentView = 'air quality'}><Wind></Wind>Air Quality</Button>
 		</div>
 
 		<!-- <AirQualityRanking items={data.airQualitySites} /> -->
-		{#if showContaminated}
+		{#if currentView === 'superfund'}
     		<ContaminatedSites items={data.contaminatedSites}/>
 		{/if}
 		
-		{#if showEmissionEvents}
+		{#if currentView === 'emission'}
+			<EmissionEvents items={data.emissionEvents}/>
+		{/if}
+
+		{#if currentView === 'air quality'}
 			<EmissionEvents items={data.emissionEvents}/>
 		{/if}
 	</aside>
-	<div>
-		<Map contaminatedSite={showContaminated ? data.contaminatedSites : null} emissionEvent={showEmissionEvents ? data.emissionEvents : null}/>
+
+	<div class="w-2/3" style="height: {height*0.8}px">
+		<Map currentView={currentView} contaminatedSite={data.contaminatedSites} emissionEvent={data.emissionEvents}/>
 	</div>
 </main>
