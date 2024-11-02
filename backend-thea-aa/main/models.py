6bbbaa07f72e2a7b352d318d346446e63552b445
nomnemo <ng53@rice.edu>
2024-11-02 15:43:26 -0500

    keeping tecq model only

--- backend-thea-aa/main/models.py
+++ backend-thea-aa/main/models.py
@@ -1,38 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 class EmissionEvents(models.Model):
 
     # Initialize fields for the Django model storing emission events
-    incident_no = models.IntegerField()
-    rn =  models.TextField()
     re_name = models.TextField()
     physical_location = models.TextField()
-<<<<<<< HEAD
-    county = models.TextField()
-    tceq_region = models.IntegerField()
-    start_date_time = models.DateTimeField()
-    end_date_time = models.DateTimeField()
-    event_type = models.TextField()
-    emission = models.TextField()
-    epn = models.TextField()
-    contaminant = models.TextField()
-    estimated_quantity = models.DecimalField()
-    estimated_ind = models.TextField()
-    amount_unk_ind = models.TextField()
-    units = models.TextField()
-    emissions_limit = models.DecimalField()
-    limit_units = models.TextField()
-    authorize_comment = models.TextField()
-    comment_no = models.IntegerField()
-    cause_of_emission = models.TextField()
-    action_taken = models.TextField()
-    basis_used = models.TextField()
-    initial_noti_date = models.DateTimeField()
-    hours_elapsed = models.DecimalField()
-    emissions_rate = models.DecimalField()
-    flag = models.BinaryField()
-=======
     start_date_time = models.TextField()
     end_date_time = models.TextField()
     contaminant = models.TextField()
@@ -40,6 +14,5 @@ class EmissionEvents(models.Model):
     emissions_limit = models.TextField()
     limit_units = models.TextField()
     hours_elapsed = models.TextField()
-    emissions_rate = models.TextField()
+    emissions_rate = models.TextField()    
     flag = models.IntegerField()
->>>>>>> ede402e844ddbcf4fcea6c1140d5b311551b394f
