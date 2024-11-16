c43c87ede37ec9b9311632a26b319865598353cc
James Loh <lohjames16@gmail.com>
2024-11-16 13:52:40 -0600

    formatting

--- frontend-thea-aa/src/routes/detailedContaminate/+page.svelte
+++ frontend-thea-aa/src/routes/detailedContaminate/+page.svelte
@@ -57,9 +57,9 @@
 				<p class="font-bold">Station</p>
 				<!-- use for each here for real thing -->
 
-				<NearAq name='Houston North Wayside C405' quality={32.07} />
-				<NearAq name='Houston Bayland Park' quality={64.04} />
-				<NearAq name='Vleux Carre Drive' quality={270.4} />
+				<NearAq name="Houston North Wayside C405" quality={32.07} />
+				<NearAq name="Houston Bayland Park" quality={64.04} />
+				<NearAq name="Vleux Carre Drive" quality={270.4} />
 
 				<p class="mt-8 text-xl font-bold">Emission Events in the Area</p>
 				<p class="mt-3">Brazos Amine Treater Facility</p>
@@ -68,20 +68,20 @@
 		</div>
 		<div class="ml-10 mr-10 w-full rounded bg-muted p-6">
 			<div class="flex flex-row">
-				<QualityIndicator quality={39.65} name='NPL' />
+				<QualityIndicator quality={39.65} name="NPL" />
 				<p class="pb-2 pl-4 text-xl font-bold">Site Overview</p>
 			</div>
 			<div class="grid grid-cols-4 gap-4">
-				<Statistic title='Hazard Level' stat={contaminatedSite.hazardScore} />
-				<Statistic title='Region ID' stat={contaminatedSite.regionID} />
-				<Statistic title='EPA Site ID' stat={contaminatedSite.siteID} />
-				<Statistic title='Location' stat={contaminatedSite.locationName} />
-				<Statistic title='Latitude' stat={contaminatedSite.location.lat} />
-				<Statistic title='Longitude' stat={contaminatedSite.location.long} />
-				<Statistic title='Construction Completion' stat={contaminatedSite.constructionComplete} />
-				<Statistic title='Partial Deletion' stat={contaminatedSite.partialDeletion} />
-				<Statistic title='Proposed' stat={contaminatedSite.proposedDate} />
-				<Statistic title='Listing' stat={contaminatedSite.listingDate} />
+				<Statistic title="Hazard Level" stat={contaminatedSite.hazardScore} />
+				<Statistic title="Region ID" stat={contaminatedSite.regionID} />
+				<Statistic title="EPA Site ID" stat={contaminatedSite.siteID} />
+				<Statistic title="Location" stat={contaminatedSite.locationName} />
+				<Statistic title="Latitude" stat={contaminatedSite.location.lat} />
+				<Statistic title="Longitude" stat={contaminatedSite.location.long} />
+				<Statistic title="Construction Completion" stat={contaminatedSite.constructionComplete} />
+				<Statistic title="Partial Deletion" stat={contaminatedSite.partialDeletion} />
+				<Statistic title="Proposed" stat={contaminatedSite.proposedDate} />
+				<Statistic title="Listing" stat={contaminatedSite.listingDate} />
 			</div>
 			<div class="flex flex-col pt-8">
 				<p class="pb-5 text-xl font-bold">Background</p>
